0821f4fd40a84825f9f8f0a0e128123dccc801bc
AngelCalderon <159507478+ELCommanderAngel@users.noreply.github.com>
2024-08-30 18:45:07 -0700

    Calderon Montoya Angilberto 5AVP 30/08/2024

---
+++ Parcial1/holamundo.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$nombre = "Angel";
+$numero = 4;
+$numero2 = "4";
+
+$total = $numero + $numero2;
+
+echo "Total =" , $total;
+
+echo "<h1> HOLA CLICKERS </h1>";
+
+for($i=0; $i<10; $i++){
+    echo $i . "<br>";
+}
+
+echo "<h3 style='color: blue;'Calderon Angel</h3>";
+
+?>
